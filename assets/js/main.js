810931e80b135bbcb6579834b73570c2cbbb72d9
Kidus <mail@kidus.ca>
2024-04-19 18:23:46 -0400

    complete overhaul with brand new design

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,12 @@
-const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    direction: 'horizontal',
-    loop: true,
-
-    // Navigation arrows
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
-    
+jQuery(document).ready(function($) {
+   if ($('.ds-testimonials-section').length) {
+		$('.ds-testimonials-slider').slick({
+		  	infinite: true,
+		  	arrows: true,
+		  	autoplay: true,
+		  	autoplaySpeed: 4000,
+		  	prevArrow:"<button type='button' class='slick-prev slick-arrow'><i class='ri-arrow-left-line'></i></button>",
+		  	nextArrow:"<button type='button' class='slick-next slick-arrow'><i class='ri-arrow-right-line'></i></button>"
+		});
+    }
 });
