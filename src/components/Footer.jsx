dc316ed1d37172cc42f4045dae33a4a12a7b218c
Kidus <mail@kidus.ca>
2024-11-05 18:02:44 -0500

    added: page footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,14 @@
 function Footer()
 {
     return (
-        <footer className="footer">
+        <footer className="footer mt-5">
+            <div className="container">
+                <div className="row">
+                    <div className="col-lg-12 mx-auto text-center">
+                        <p>&copy; 2024 &mdash; Built with 💖 and ☕!</p>
+                    </div>
+                </div>
+            </div>
         </footer>
     )
 }
